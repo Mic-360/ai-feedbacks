cd0ddfacfad0bfcc9736675edbacd84cca612b4f
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2026-04-27 15:24:51 +0530

    Simplify chat launcher imports

--- components/chat/ChatLauncher.tsx
+++ components/chat/ChatLauncher.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { MessageCircle } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { ChatModal } from "./ChatModal";
 
 export function ChatLauncher() {
